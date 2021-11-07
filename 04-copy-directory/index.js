c26a4c73a084de368b6f5f47ea4e7030ae8cc27d
Klimentsii <59070287+klimentsii@users.noreply.github.com>
2021-11-08 01:11:40 +0300

    gg

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,19 @@
+const fs = require('fs');
 
+fs.readdir('04-copy-directory/files-copy/', (err, files) => {
+  if (err) throw err;
+
+  for (const file of files) {
+    fs.unlink(require('path').join('04-copy-directory/files-copy/', file), err => {
+      if (err) throw err;
+    });
+  }
+});
+
+copyDir(require('ncp').ncp)
+
+function copyDir(ncp) {
+    ncp('04-copy-directory/files/', '04-copy-directory/files-copy/', function (err) {
+        if (err) { return console.error(err); }
+    });
+}
