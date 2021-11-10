1199d4d696e457ca31924466216e33896e4dd33e
Klimentsii <59070287+klimentsii@users.noreply.github.com>
2021-11-10 20:17:39 +0300

    fix: remove part of path

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 
-fs.readdir(require('path').join(__dirname, '03-files-in-folder/secret-folder'), { withFileTypes: true }, (err, items) => {
+fs.readdir(require('path').join(__dirname, 'secret-folder'), { withFileTypes: true }, (err, items) => {
   if(err) throw err;
 
   items.forEach(item => {
     if (item.isFile()) {
-      fs.stat(require('path').join(__dirname, '03-files-in-folder/secret-folder', item.name), (err, stats) => {
+      fs.stat(require('path').join(__dirname, 'secret-folder', item.name), (err, stats) => {
         if(err) throw err;
         else {
           console.log(`${require('path').parse(item.name).name} - ${require('path').parse(item.name).ext.slice(1)} - ${stats.size}b`);
