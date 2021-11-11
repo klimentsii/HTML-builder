3e60bedf04d13b74a06bc0f0841e714158969607
Klimentsii <59070287+klimentsii@users.noreply.github.com>
2021-11-11 11:46:12 +0300

    fix:

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,17 +1,24 @@
-const fsPromises = require('fs/promises');
+const fs = require('fs');
+const fsPromises = fs.promises;
 const path = require('path');
 
-fsPromises.mkdir(path.join(__dirname, 'files-copy'), {recursive: true});
+fs.rm(path.join(__dirname, 'files-copy'), {force: true, recursive: true}, (err) => {
+    if (err) console.error(err.message);
 
-async function copyDir() {
-    const files = await fsPromises.readdir(path.join(__dirname,'files'),{withFileTypes: true});
-
-    for (const file of files) {
-        if (file.isFile()) {
-            fsPromises.copyFile( path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name));
-        } else {
-            fsPromises.mkdir(path.join(__dirname, 'files-copy' ,file.name), {recursive: true});
-        }
-    }
-}
-try { copyDir(); } catch (err) { throw err }
+    fsPromises.mkdir(path.join(__dirname, 'files-copy'), {recursive: true})
+    .then( () => fsPromises.readdir(path.join(__dirname, 'files'), {withFileTypes: true}) )
+    .then(data => {
+        data.forEach(dirent => {
+            if (dirent.isFile()) {
+                const pathToFile = path.join(path.join(__dirname, 'files'), dirent.name);
+                const pathToFileCopy = path.join(path.join(__dirname, 'files-copy'), dirent.name);
+                fsPromises.copyFile(pathToFile, pathToFileCopy);
+            } else if (dirent.isDirectory()) {
+                const pathToFolder = path.join(path.join(__dirname, 'files'), dirent.name);
+                const pathToFolderCopy = path.join(path.join(__dirname, 'files-copy'), dirent.name);
+                fsPromises.mkdir(pathToFolderCopy, {recursive: true});
+            }
+        })
+    })
+    .catch(err => console.error(err.message));
+})
